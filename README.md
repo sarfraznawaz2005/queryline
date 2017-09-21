02ef8176f8408d78832e677b271856a0140a1a66
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2017-09-21 15:30:22 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Laravel QueryLine
+
+[![laravel 5.1](https://img.shields.io/badge/Laravel-5.1-brightgreen.svg?style=flat-square)](http://laravel.com)
+[![laravel 5.2](https://img.shields.io/badge/Laravel-5.2-brightgreen.svg?style=flat-square)](http://laravel.com)
+[![laravel 5.3](https://img.shields.io/badge/Laravel-5.3-brightgreen.svg?style=flat-square)](http://laravel.com)
+[![laravel 5.4](https://img.shields.io/badge/Laravel-5.4-brightgreen.svg?style=flat-square)](http://laravel.com)
+[![downloads](https://poser.pugx.org/sarfraznawaz2005/queryline/downloads)](https://packagist.org/packages/sarfraznawaz2005/eventable)
+
+## Introduction ##
+
+QueryLine is a laravel package to show time graph against run quries on a page thereby allowing to see which are slow/fastest quries running on the page.
+
+## Screenshot ##
+
+![Main Window](https://raw.github.com/sarfraznawaz2005/queryline/master/screen.png)
+
+## Requirements ##
+
+ - PHP >= 5.6
+ - Laravel 5
+
+## Installation ##
+
+Install via composer
+
+```
+composer require sarfraznawaz2005/queryline
+```
+
+Add Service Provider to `config/app.php` in `providers` section
+```php
+Sarfraznawaz2005\QueryLine\ServiceProvider::class,
+```
+
+Run `php artisan vendor:publish` to publish package's config file. You should now have `queryline.php` file published in `app/config` folder.
+
+## Config Options ##
+
+ - `enabled` : Enable or disable QueryLine. By default it is disabled. If you are on local environment, you can also just add `QUERYLINE=true` to env file to enable it.
+ - `querystring_name` : Whatever value for this config is set, you will be able to see all running quries by appending this value in your url as query string. Example: `http://www.yourapp.com/someurl?vvv`. Default value is `vvv`.
+ 
+## Related Package ##
+
+[QueryDumper](https://github.com/sarfraznawaz2005/querydumper)
+
+## License ##
+
+This code is published under the [MIT License](http://opensource.org/licenses/MIT).
+This means you can do almost anything with it, as long as the copyright notice and the accompanying license file is left intact.
---
+++ src/ServiceProvider.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Sarfraznawaz2005\QueryLine;
+
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    private static $counter = 0;
+    private static $dataString = '';
+
+    public function boot()
+    {
+        // publish our files over to main laravel app
+        $this->publishes([
+            __DIR__ . '/config/queryline.php' => config_path('queryline.php')
+        ]);
+
+        if (!$this->isEnabled()) {
+            return;
+        }
+
+        $this->prepare();
+
+        DB::listen(function ($sql, $bindings = null, $time = null) {
+            if ($sql instanceof QueryExecuted) {
+                $time = $sql->time;
+                $bindings = $sql->bindings;
+                $sql = $sql->sql;
+            }
+
+            $sql = $this->applyBindings($sql, $bindings);
+
+            $count = ++self::$counter;
+
+            self::$dataString .= "[$count, $time, \"$sql\"],\n";
+
+            $this->output();
+        });
+    }
+
+    protected function output()
+    {
+        $data = self::$dataString;
+
+        echo <<< SCRIPT
+        
+    <script>
+    google.charts.load('current', {'packages':['corechart']});
+    
+    google.charts.setOnLoadCallback(function() {
+      window.QueryLineDataTable = new google.visualization.DataTable();
+      
+      window.QueryLineDataTable.addColumn('number', 'Time');
+      window.QueryLineDataTable.addColumn('number', 'Query');
+      window.QueryLineDataTable.addColumn({type: 'string', role: 'tooltip'});
+      
+      window.QueryLineDataTable.addRows([$data]);
+    
+      window.QueryLineDataTable.options = {"legend": "none", "title": "QueryLine", "vAxis": {"format": "#ms", title: "Time"}, "hAxis": {"format": "#", title: "Query", "minValue": 1, "maxValue": 5}, "width": "100%", "height":600};
+      window.QueryLineDataTable.chart = new google.visualization.ColumnChart(document.getElementById('__queryline_chart__'));
+      
+      window.QueryLineDataTable.chart.draw(window.QueryLineDataTable, window.QueryLineDataTable.options);
+    });     
+    </script>
+        
+SCRIPT;
+
+    }
+
+    protected function prepare()
+    {
+        $html = <<< HTML
+        
+    <div id="__queryline_chart__" style="margin: 0 auto;"></div>
+    
+    <script type="text/javascript" src="//www.gstatic.com/charts/loader.js"></script>
+
+HTML;
+
+        echo $html;
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    protected function isEnabled()
+    {
+        $enabled = config('queryline.enabled');
+
+        if (!$enabled) {
+            return false;
+        }
+
+        $queryString = config('queryline.querystring_name');
+
+        if ($this->app->runningInConsole()) {
+            return in_array($queryString, $_SERVER['argv']);
+        }
+
+        return request()->exists($queryString);
+    }
+
+    protected function applyBindings($sql, array $bindings)
+    {
+        if (empty($bindings)) {
+            return $sql;
+        }
+
+        $placeholder = preg_quote('?', '/');
+
+        foreach ($bindings as $binding) {
+            $binding = is_numeric($binding) ? $binding : "'{$binding}'";
+            $sql = preg_replace('/' . $placeholder . '/', $binding, $sql, 1);
+        }
+
+        return $sql;
+    }
+}
